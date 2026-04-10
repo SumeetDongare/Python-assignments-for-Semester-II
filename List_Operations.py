6ea2e34019c3e6c5d85e996f01f76f669b44bdf6
Sumeet Dongare <Sumeet.dongare19@gmail.com>
2026-04-10 18:54:46 +0530

    Add files via upload

---
+++ List_Operations.py
@@ -0,0 +1,19 @@
+# creating list:
+my_list = [10, 20, 30, 40]
+print(my_list)
+# Accessing elements:
+print(my_list[0])
+print(my_list[:3])
+# Modifying elements:
+my_list[0] = 10
+print(my_list)
+# Adding elements:
+my_list.append(50)
+print(my_list)
+my_list.insert(1, 5)
+print(my_list)
+# Removing elements:
+my_list.pop()
+print(my_list)
+my_list.remove(20)
+print(my_list)
